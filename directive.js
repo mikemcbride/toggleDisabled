3595dc8e5e27a0c7e3c14de1a115c3ad6beed9e8
Mike McBride <mmcbride1007@gmail.com>
2014-04-30 09:00:05 -0500

    Create directive.js

---
+++ directive.js
@@ -0,0 +1,15 @@
+app.directive('toggleDisabled', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            scope.$watch(attrs.toggleDisabled, function (new_value, old_value) {
+                if (new_value) {
+                    element.removeAttr('disabled');
+                }
+                else {
+                    element.attr('disabled', 'disabled');
+                }
+            });
+        }
+    };
+});
